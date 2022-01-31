69e96edd7cc5dbe1287350e58b99e3700000134a
josephdaw <88129605+josephdaw@users.noreply.github.com>
2022-01-31 12:14:57 +1030

    fixing anchor link scrolling too far

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container'
 
 export default function TopNav() {
     return (
-        <Navbar collapseOnSelect expand="md" bg="primary" variant="dark" sticky='top'>
+        <Navbar collapseOnSelect expand="md" bg="primary" variant="dark">
             <Container>
                 <Navbar.Brand href="#home">Joseph Daw</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
