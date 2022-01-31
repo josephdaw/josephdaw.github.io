0cb84f5f1953cef954171677cd3bb491aac6303d
josephdaw <88129605+josephdaw@users.noreply.github.com>
2022-01-31 11:49:56 +1030

    updating nav links

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
 <Row id="about" xs={1} md={2} lg={3} className="g-4">
     <Col>
         <h2>Hi, my name is Joe</h2>
-        <h3>A full stack web developer with a background in Aviation</h3>
+        <h4>A full stack web developer with a background in Aviation</h4>
     </Col>
 </Row>
   );
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -14,13 +14,7 @@ export default function TopNav() {
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#about">About Me</Nav.Link>
-                        <NavDropdown title="portfolio" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#portfolio">Portfolio</Nav.Link>
                     </Nav>
                     <Nav>
                         <Nav.Link href="#contact">Contact Me</Nav.Link>
