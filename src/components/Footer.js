7376fad0fbd3924b50a41b7f480988100945c153
josephdaw <josephdaw@icloud.com>
2022-01-31 22:33:17 +1030

    updating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,27 @@ import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { Col, Row } from "react-bootstrap";
 
 
 export default function Footer() {
     return (
-        <Navbar collapseOnSelect expand="md" bg="light" variant="light">
-            <Container>
-            <FontAwesomeIcon icon={faGithub} />
-            <FontAwesomeIcon icon={faTwitter} />
-            <FontAwesomeIcon icon={faLinkedin} />
+        <Container id='footer'>
+            <Container style={{display: 'flex', justifyContent: 'space-around'}}>
+                <a className="grey-text text-lighten-3" href="http://www.github.com/josephdaw" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a className="grey-text text-lighten-3" href="https://twitter.com/Joseph_Daw" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faTwitter} />
+                </a>
+                <a className="grey-text text-lighten-3" href="https://www.linkedin.com/in/joseph-daw"
+                    target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </a>
             </Container>
-        </Navbar>
+            <Container style={{display: 'flex', justifyContent: 'center'}}>
+                &copy; 2021 Joseph Daw
+            </Container>
+        </Container>
     );
 }
