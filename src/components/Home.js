dc454bf766fecda07a3406d7ed1b3d944b74f52b
josephdaw <josephdaw@icloud.com>
2022-02-02 12:39:24 +1030

    adding image to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import { Link } from "react-router-dom";
-
+import Joseph from '../img/joseph.jpg';
 
 function Home() {
   return (
     <>
         <h2>Hi, my name is Joe</h2>
         <h4>A full stack web developer with a background in Aviation</h4>
-        <p>Learn more: <Link to="about">About Me</Link></p>
+        <p>Click here to learn more <Link to="about">about me</Link></p>
+        <img src={Joseph} alt="picture of Joseph" style={{maxHeight: '30vh'}}/>
     </>
   );
 }
